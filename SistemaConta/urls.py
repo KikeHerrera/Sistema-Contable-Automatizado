b34510f00677b0fa14cd977c889c72c05456ec7c
Kike <hh22035@ues.edu.sv>
2024-10-28 17:23:29 -0600

    Inicio completo y funcional

--- SistemaConta/urls.py
+++ SistemaConta/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     #arreglar el inicio
     path('', views.prueba_view, name='index'),
     path('index/', views.index_view, name='index'),
-    path('prueba/', views.prueba_view, name='prueba'),
 ]
 
