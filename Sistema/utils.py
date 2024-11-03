562e475dfed1a6e5185af71cbf04388af0718ce0
PG22010 <rodrigogarciaguirre@gmail.com>
2024-11-03 09:56:28 -0600

    db

--- Sistema/utils.py
+++ Sistema/utils.py
@@ -10,10 +10,10 @@ def filtrar_o_crear_partida_diaria():
 
     # Si no existe una partida diaria para hoy, crear una nueva
     if not partida_diaria:
-        libro_mayor_actual = LibroMayor.objects.get_or_create(id_libro_mayor=1)
+        libro_mayor_actual, created = LibroMayor.objects.get_or_create(id_libro_mayor=1)
         partida_diaria = PartidaDiaria.objects.create(
             fecha=fecha_hoy,
-            id_libro_mayor=libro_mayor_actual
+            id_libro_mayor=libro_mayor_actual  # Usa solo el objeto, no la tupla
         )
     
     return partida_diaria
