62450a0a107e21e51fb55248fa15ba42ef4ffe9e
PG22010 <rodrigogarciaguirre@gmail.com>
2024-11-03 21:55:13 -0600

    Transacciones de prueba

--- Sistema/admin.py
+++ Sistema/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import CuentaContable, BalanceGeneral, CuentasBalanceGeneral, EstadoDeResultado, EstadoDeCapital
+from .models import Asiento, Transaccion, PartidaDiaria, CuentaContable, BalanceGeneral, CuentasBalanceGeneral
 
 # Registrar cada uno de los modelos para que aparezcan en el panel de administración
 admin.site.register(CuentaContable)
@@ -9,3 +10,6 @@ admin.site.register(BalanceGeneral)
 admin.site.register(CuentasBalanceGeneral)
 admin.site.register(EstadoDeResultado)
 admin.site.register(EstadoDeCapital)
+admin.site.register(Asiento)
+admin.site.register(Transaccion)
+admin.site.register(PartidaDiaria)
