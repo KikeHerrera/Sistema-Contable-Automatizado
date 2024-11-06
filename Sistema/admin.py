73e6099af9a16bcd21f533e431a8050173939d0e
PG22010 <rodrigogarciaguirre@gmail.com>
2024-11-05 20:53:18 -0600

    Sistema Contable

--- Sistema/admin.py
+++ Sistema/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import CuentaContable, BalanceGeneral, CuentasBalanceGeneral, EstadoDeResultado, EstadoDeCapital
-from .models import Asiento, Transaccion, PartidaDiaria, CuentaContable, BalanceGeneral, CuentasBalanceGeneral
+from .models import Asiento, Transaccion, PartidaDiaria, CuentaContable, BalanceGeneral, CuentasBalanceGeneral, Empleado
 
 # Registrar cada uno de los modelos para que aparezcan en el panel de administración
-admin.site.register(CuentaContable)
 admin.site.register(BalanceGeneral)
 admin.site.register(CuentasBalanceGeneral)
 admin.site.register(EstadoDeResultado)
 admin.site.register(EstadoDeCapital)
-admin.site.register(Asiento)
-admin.site.register(Transaccion)
 admin.site.register(PartidaDiaria)
+admin.site.register(Empleado)
